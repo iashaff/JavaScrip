76491f7b7e1d50641b6824c28dafdf5eedd56dc6
unknown <optimistka30@mail.ru>
2022-05-05 22:28:10 +0300

    Added rendering basket

--- dom 6/basket/script.js
+++ dom 6/basket/script.js
@@ -1,59 +1,78 @@
 var bsk = []; //массив для корзины
-var a = document.getElementsByClassName("name");
-var b = document.getElementsByClassName("price");
+var btn = document.querySelectorAll("btn");
+var renderedBsk = document.getElementById("bas-ket");
+var goods = document.getElementById("list");
+var total = document.getElementById("total");
 
-//Определяем именно тот товар где нажали кнопку купить
-function buy() {
-    switch (event.target) {
-    case i1:
-        bsk.push(tovars[0]);
-        break;
-    case i2:
-        bsk.push(tovars[1]);
-        break;
-    case i3:
-        bsk.push(tovars[2]);
-        break;
-    }
-    console.log(bsk);
-}
 
 //Массив товаров
-var tovars = [
+var catalog = [
     {
-        title: a, 
-        price: b, 
-        quantity: 1
-},
-     {
-        title: a,
-        price: b,
-        quantity: 1
-},
-     {
-        title: a,
-        price: b,
-        quantity: 1
-}
+        title: "Фотоаппарат",
+        price: 15000,
+        image: "./img/1.jpg",
+        id: 1
+    },
+    {
+        title: "Наушники",
+        price: 7000,
+        image: "./img/2.jpg",
+        id: 2
+    },
+    {
+        title: "Телефон",
+        price: 10000,
+        image: "./img/3.png",
+        id: 3,
+    }
 ];
-//Выдергиваем из ключей название товарар и цену.
-var i = 0;
-   for (tovar of tovars) {
-        tovar.title = a [i].innerHTML;
-       tovar.price = b [i].innerHTML;
-       i++;       
-    }   
 
-/*function countBasketPrice(tovars) {
-            for (tovar of tovars) {
-                tovar.quantity = +prompt("Введите колличество ");
-                var p = 0;
-                p = (p + tovar.price) * tovar.quantity;
-                c = c + p;
-            }
-            return c;
+//Отрисовываем товары на странице
+
+function renderGoods() {
+    for (var i = 0; i < catalog.length; i++) {
+        var item = document.createElement('div');
+        item.className = 'items';
+        item.innerHTML = ` <img src="${catalog[i].image}" />
+                            <h2> ${catalog[i].title} </h2>
+                             <p> ${catalog[i].price} руб.</p>
+                              <button class="btn" data-id = ${catalog[i].id}>Купить</button>`
+
+        goods.appendChild(item)
+
+    }
+
+}
+renderGoods();
+
+//Определяем именно тот товар где нажали кнопку купить и добавляем в массив корзины
+
+
+
+document.onclick = (event) => {
+    if (event.target.classList.contains('btn')) {
+        addGoodsInBasket(event.target.dataset.id)
+    }
+
+
+}
+// Добавляем выбранный товар в массив и отрисовываем корзину с общей суммой
+const addGoodsInBasket = id => {
+    var sum = 0
+    catalog.forEach(element => {
+        if (event.target.dataset.id == element.id) {
+            bsk.push({ title: element.title, price: element.price, count: 1 })
         }
-console.log(countBasketPrice(tovars));*/
 
+    })
+    for (let i = 0; i < bsk.length; i++) {
+        sum += bsk[i].price
+    }
 
+    renderedBsk.innerHTML += `<ul>
+        <li>${bsk[bsk.length - 1].title} ${bsk[bsk.length - 1].price} руб.</li>
+        </ul>`
+    total.textContent = `Итого: ${sum} руб.`
+
+}
 
